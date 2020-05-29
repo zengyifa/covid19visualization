84edea492acb6a8f7f62e8ba990f7aeecc3a3468
zengyifa <zengyifa@umich.edu>
2020-05-28 20:16:17 -0400

    next day function

--- util.js
+++ util.js
@@ -1,3 +1,39 @@
 function getBaseLog(x, y) {
     return Math.log(y) / Math.log(x);
 }
+
+/**
+ * distributes value into array of length length
+ * @param value to distribute
+ * @return array of length length with randomly distributed value
+ */
+function distribute(length, value) {
+    if (length <= 1)
+        return [value];
+    var half = Math.floor(length / 2),
+        dist = Math.floor(Math.random() * value);
+    return distribute(half, dist).concat(distribute(length-half, value-dist));
+}
+
+/**
+ * distributes value to array
+ */
+function distribute_add(array, value) {
+    var to_be_added = distribute(array.length, value);
+    return array.map(function (num, idx) {
+        return num + to_be_added[idx];
+    });
+}
+
+/**
+ * distributes value to values in dictionary
+ */
+function distribute_add_dict(dict, value) {
+    var to_be_added = distribute(Object.keys(dict).length, value);
+    var arrayIndex = 0;
+    for (var dictKey in dict) {
+        dict[dictKey] += to_be_added[arrayIndex];
+        arrayIndex += 1;
+    }
+    return dict;
+}
